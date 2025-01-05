5c2170bd04dd45adf4b64b0f7665e9b8f1088e3d
NISHAN75 <nishandas880@gmail.com>
2025-01-05 16:32:12 +0600

    reatangle is ok

--- js/rectangle.js
+++ js/rectangle.js
@@ -1,3 +1,17 @@
 function calculateReatAngleArea(){
-    console.log("contact")
+
+    const lengthInput = document.getElementById("rectangle-length");
+    const lengthText = lengthInput.value;
+    const length = parseFloat(lengthText);
+    const widthInput = document.getElementById("rectangle-width");
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
+    if (isNaN(length) || isNaN(width)) {
+        alert("Please enter valid numbers for length and width.");
+        return;
+    }
+    const area = length * width;
+    const rectangleSpan = document.getElementById("rectangle-span");
+    rectangleSpan.innerText = area;
+
 }
